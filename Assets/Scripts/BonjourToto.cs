00a12c4898bb34464dd23ef29bdd6ae51fd8a440
Furi3NTM <116046227+Furi3NTM@users.noreply.github.com>
2023-02-17 12:28:42 -0500

    Initial commit

---
+++ Assets/Scripts/BonjourToto.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonjourToto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Bonjour Toto!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
